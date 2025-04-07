0a3a0165392700143006f4152b0c4acc632e2bbb
lokrim <ahmedzyed123@gmail.com>
2025-04-07 17:55:39 +0530

    updated firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOHpt-mtrl7gzM6z21RN6pGCDLeF4GKuE",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "class-clarity-4748d.firebaseapp.com",
   projectId: "class-clarity-4748d",
   storageBucket: "class-clarity-4748d.firebasestorage.app",
@@ -19,5 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
